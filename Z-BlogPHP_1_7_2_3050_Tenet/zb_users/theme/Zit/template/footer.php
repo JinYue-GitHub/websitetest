c90e9e7fb6de766674b8eb7d860aa1f079c30328
JinYue-GitHub <jinyue8415@gmail.com>
2022-09-26 00:59:38 +0800

    zblog-拓源主题
    去版权

---
+++ Z-BlogPHP_1_7_2_3050_Tenet/zb_users/theme/Zit/template/footer.php
@@ -0,0 +1,8 @@
+<footer id="base">
+  <div class="inner">
+    <h4>&copy; {date('Y',time())} <a href="{$host}" class="zit">{$cfg.Logo?$cfg.Logo:$name}</a> {$copyright}</h4>
+    {$footer}
+  </div>
+</footer>
+</body>
+</html>
